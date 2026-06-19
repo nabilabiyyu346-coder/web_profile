9a918362c81b7c7d68ce800bc832b7ef2654bde5
Iqbass <iqbalalhafidz26@gmail.com>
2026-06-19 15:50:39 +0700

    Add files via upload

---
+++ resources/views/CV.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
+  <body>
+    <navbar class=" bg-gray-600 px-8 py-4 flex justify-between items-center">
+        <span class="text-xl font-bold tracking-wider ">Iqbal.CV</span>
+        <div class="space-x-6 text-sm font-medium">
+            <a href="" class="hover:text-blue-400 transition">Tentang</a>
+            <a href="" class="hover:text-blue-400 transition">Pengalaman</a>
+            <a href="" class="hover:text-blue-400 transition">Keahlian</a>
+            <a href="" class="hover:text-blue-400 transition">Bimbingan</a>
+        </div>
+    </navbar>
+    <tbody>
+        <div class="text xl font-bold px-8 py-2">
+
+            <span>About me</span>
+            <ul>
+                <li>Saya merupakan siswa SMKN 1 Jenangan Ponorogo jurusan Rekayasa Perangkat Lunak. Memiliki pengalaman mengerjakan
+                    proyek perangkat lunak secara individu maupun tim, serta memiliki motivasi tinggi untuk belajar, beradaptasi, dan
+                    mengembangkan kemampuan di dunia kerja melalui kegiatan PKL.</li>
+            </ul><br>
+
+            <span>Contact</span>
+            <ul class="flex items-center justify-between">
+                <div class="space-x-12 ">
+                <a href="" class="hover:text-blue-400 transition">Whatsapp</a></a>
+                <a href="" class="hover:text-blue-400 transition">Gmail</a></a>
+                <a href="" class="hover:text-blue-400 transition">Alamat</a></a>
+                </div>
+            </ul><br>
+
+            <span>Pendidikan</span>
+            <ul>
+                <li>MTsN 2 Ponorogo 2021-2024 SMKN 1 Jenangan</li>
+                <li>Ponorogo Jurusan RPL2024 - Sekarang</li>
+            </ul><br>
+
+            <span>Prestasi</span>
+            <ul>
+                <li>Juara 2 Lomba Scratch Tingkat Sekolah (Diadakan oleh mahasiswa Universitas Malang)</li>
+                <li>Terbaik 2 Lomba Arek_AI Jatim (Kategori Aplikasi Python) Kegiatan FESTIKA Jawa Timur 2025</li>
+            </ul><br>
+
+            <span>Hard Skill</span>
+            <ul>
+                <li>Python, C++, PHP (Laravel).</li>
+                <li>Logika dan algoritma pemrograman.</li>
+                <li>HTML dan Css (Dasar).</li>
+                <li>PostgreSQL (Dasar).</li>
+                <li>GitHub.</li>
+                <li>Microsoft Office, Canva, CapCut.</li>
+                <li>Editing Vidio (Capcut, Alight Motion).</li>
+            </ul><br>
+
+            <span>Hard Skill</span>
+            <ul>
+                <li>Kerja tim dan komunikasi</li>
+                <li>Disiplin dan bertanggung jawab</li>
+                <li>Problem solving dasar</li>
+                <li>Mampu menerima arahan danevaluasi</li>
+                <li>Adaptif dalam lingkungan</li>
+            </ul><br>
+        </div>
+    </tbody>
+
+
+  </body>
+</html>
