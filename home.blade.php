130ef2eb7deedd709b1ad80804ef3c9811cf769f
Kenip <kenip3765@gmail.com>
2026-06-19 16:00:33 +0700

    view data row
    tadi mengikuti tutorial ypoutube tapi yang pakai template masih error, ini saya pakai dari project cadangan

---
+++ home.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Profile</title>
+
+</head>
+<body>
+
+    <!-- HERO -->
+    <section>
+        <div>
+
+            <img src="{{ asset('\uploads\IMG-20250516-WA0001.jpg') }}" alt="Profile">
+
+            <h1>
+                Hanif Fikri Saputra
+            </h1>
+
+            <h2>
+                Profile
+            </h2>
+
+            <p> 
+                Tempat Tanggal Lahir: Ponorogo, 16 januari 2009
+
+            </p>
+
+            <p>
+                Web Developer || Student
+            </p>
+
+            <p>
+                Siswa SMKN 1 Jenangan Ponorogo jurusanRekayasa Perangkat Lunak yangbersemangat dengan kemampuan dalampengenmbangan aplikasi dan web.Memiliki pengalaman dalampengembangan proyek perangkat lunaksecara individu maupun tim. Bermotivasitinggi untuk beradaptasi danmengembangkan kemampuan di dunia kerja melalui program PKL
+
+            </p>
+
+        
+
+        </div>
+    </section>
+
+    <!-- ABOUT -->
+    <section>
+        <div>
+
+            <h2>
+                Tentang Saya
+            </h2>
+
+            <p>
+                Saya memiliki ketertarikan dalam pengembangan aplikasi web,
+                khususnya menggunakan Laravel Framework. Saat ini sayamasih terus mempelajarinya.
+            </p>
+
+        </div>
+    </section>
+
+    <!-- EDUCATION -->
+    <section>
+        <div>
+
+            <h2>
+                Pendidikan
+            </h2>
+
+            <div>
+
+                <div>
+                    <h3>
+                        SMKN 1 Jenangan
+                    </h3>
+
+                    <p>
+                        2024 - Sekarang
+                    </p>
+                </div>
+
+                <div>
+                    <h3>
+                        SMPN 5 Ponorogo
+                    </h3>
+
+                    <p>
+                        2021 - 2024
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <!-- PROJECT -->
+    <section>
+        <div>
+
+            <h2>
+                Pengalaman
+            </h2>
+
+            <div>
+
+                <div>
+                    <h3>
+                        Mengikuti Program PKL di pt promedia citra digital informatika
+                    </h3>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <!-- CONTACT -->
+    <section>
+        <div>
+
+            <h2>
+                Kontak
+            </h2>
+
+            <div>
+                <p>Email : haniffikrisaputra@gmail.com</p>
+                <p>WhatsApp : +62 838 7208 7602</p>
+            </div>
+
+        </div>
+    </section>
+
+</body>
+</html>
